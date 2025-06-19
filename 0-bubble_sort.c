ddfe65e17ad041631ff1969b709e404fc9f30802
AbdullahALsanie <Ab.Alsanie4@gmail.com>
2025-06-19 12:17:58 +0000

    fixed betty coding style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,31 @@
 #include "sort.h"
 /**
- * bubble_sort - sorts an array of integers in ascending order using the Bubble sort algorithm
+ * bubble_sort - sorts an array of integers using Bubble sort
  * @array: the array to sort
  * @size: the array size
  * Return: nothing
  */
 void bubble_sort(int *array, size_t size)
 {
-        size_t i, j;
-        int tmp, flag;
+	size_t i, j;
+	int tmp, flag;
 
-        for (i = 0; i <= size-1; i++)
-        {
-                flag = 0;
+	for (i = 0; i <= size - 1; i++)
+	{
+		flag = 0;
 
-                for(j = 0; j < size - i - 1; j++)
-                {
-                        if (array[j] > array[j+1])
-                        {
-                        tmp = array[j+1];
-                        array[j+1] = array[j];
-                        array[j] = tmp;
-                        print_array(array, size);
-                        flag = 1;
-                        }
-                }
-                if (flag == 0)
-                        break;
-        }
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+			break;
+	}
 }
